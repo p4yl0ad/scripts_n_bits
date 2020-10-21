2d8141adbc862e5ddf9d813c13291adbdba00155
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2020-10-21 15:47:47 +0100

    Update smtp_vrfypy27.py

--- smtp_vrfypy27.py
+++ smtp_vrfypy27.py
@@ -3,23 +3,22 @@
 import socket
 import sys
 
-ip_start = '10.11.1.'
-
 if len(sys.argv) != 2:
     print "Usage: vrfy.py <username>"
     sys.exit(0)
 
-for i in range(0,256):
-    ip = ip_start + str(i)
-    print "VRFY root against the ip: " + ip
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        connect = s.connect(( ip , 25 ))
-        banner = s.recv(1024)
-        print banner
-        s.send('VRFY ' + sys.argv[1] + '\r\n')
-        result = s.recv(1024)
-        print result
-        s.close()
-    except:
-        print "failed against IP: " + ip
+with open('SMTP-ip-addresses.txt', 'r') as ipl:
+    ips = (line.rstrip() for line in ipl) #ipl.readlines()
+    for i in ips:
+        print "VRFY root against the ip: " + i
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            connect = s.connect(( i , 25 ))
+            banner = s.recv(1024)
+            print banner
+            s.send('VRFY ' + sys.argv[1] + '\r\n')
+            result = s.recv(1024)
+            print result
+            s.close()
+        except:
+            print "failed against IP: " + i
